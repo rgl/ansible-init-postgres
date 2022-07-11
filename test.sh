9774ba51d55389216816b216657bdc425c0cf605
Rui Lopes <rgl@ruilopes.com>
2022-07-11 07:11:49 +0100

    test connecting to the created database

--- test.sh
+++ test.sh
@@ -5,22 +5,18 @@ set -euo pipefail
 docker compose down
 docker compose up --build --detach
 
-# wait for the init service to exit.
-while true; do
-result="$(docker compose ps --status exited --format json init)"
-if [ -n "$result" ] && [ "$result" != 'null' ]; then
-    exit_code="$(jq -r '.[].ExitCode' <<<"$result")"
-    break
-fi
-sleep 3
-done
-
-# output the init service logs.
-docker compose stop
-docker compose logs init
-
-# fail with an error when the test failed.
-if [ "$exit_code" != "0" ]; then
-    echo "ERROR test failed with exit code $exit_code"
-    exit 1
-fi
+# wait for the init and test services to exit.
+function wait-for-service {
+    while true; do
+        result="$(docker compose ps --status exited --format json $1)"
+        if [ -n "$result" ] && [ "$result" != 'null' ]; then
+            exit_code="$(jq -r '.[].ExitCode' <<<"$result")"
+            break
+        fi
+        sleep 3
+    done
+    docker compose logs $1
+    return $exit_code
+}
+wait-for-service init
+wait-for-service test
