825a91a8a093127855b25184ee4cd42b9ce486c5
Rui Lopes <rgl@ruilopes.com>
2023-12-10 10:00:37 +0000

    update wait-for-service to work with latest docker compose
    tested with docker compose 2.23.3

--- test.sh
+++ test.sh
@@ -13,9 +13,9 @@ docker compose up --build --detach
 function wait-for-service {
     echo "waiting for the $1 service to exit..."
     while true; do
-        result="$(docker compose ps --status exited --format json $1)"
+        result="$(docker compose ps --all --status exited --format json $1)"
         if [ -n "$result" ] && [ "$result" != 'null' ]; then
-            exit_code="$(jq -r '.[].ExitCode' <<<"$result")"
+            exit_code="$(jq -r '.ExitCode' <<<"$result")"
             break
         fi
         sleep 3
